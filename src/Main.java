9af0dd1964154716abf10cd6bed0231d6d725147
andreghirotti <contato.ag100@gmail.com>
2025-12-03 18:05:57 -0300

    Heranca e polimorfismo parte II

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descricao curso Java");
+        curso1.setCargaHoraria(8);
+        System.out.println(curso1);
+
+
+        Mentoria mentoria = new Mentoria();
+
+        System.out.println(mentoria);
+    }
+}
